e2bb47cbad7993f2d4ad30e018584bbe61687615
Anastasia Isaeva <anastasiaisaeva@Anastasias-MBP.attlocal.net>
2021-09-26 13:32:09 -0500

    step 5 add the comment-form

--- view-post.php
+++ view-post.php
@@ -10,18 +10,43 @@ if (isset($_GET['post_id']))
 else
 {
     // So we always have a post ID var defined
-    $postId = 1;
+    $postId = 0;
 }
 
 // Connect to the database, run a query, handle errors
 $pdo = getPDO();
 $row = getPostRow($pdo, $postId);
 
+// If the post does not exist, let's deal with that here
+if (!$row)
+{
+    redirectAndExit('index.php?not-found=1');
+}
+
+$errors = null;
+if ($_POST)
+{
+    $commentData = array(
+        'name' => $_POST['comment-name'],
+        'website' => $_POST['comment-website'],
+        'text' => $_POST['comment-text'],
+    );
+    $errors = addCommentToPost(
+        $pdo,
+        $postId,
+        $commentData
+    );
+
+    // If there are no errors, redirect back to self and redisplay
+    if (!$errors)
+    {
+        redirectAndExit('view-post.php?post_id=' . $postId);
+    }
+}
 
 // Swap carriage returns for paragraph breaks
 $bodyText = htmlEscape($row['body']);
 $paraText = str_replace("\n", "</p><p>", $bodyText);
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -33,10 +58,11 @@ $paraText = str_replace("\n", "</p><p>", $bodyText);
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 </head>
 <body>
-    <?php require 'templates/title.php' ?>
-    <h2>
-        <?php echo htmlEscape($row['title']) ?>
-    </h2>
+<?php require 'templates/title.php' ?>
+
+<h2>
+    <?php echo htmlEscape($row['title']) ?>
+</h2>
 <div>
     <?php echo convertSqlDate($row['created_at']) ?>
 </div>
@@ -45,21 +71,24 @@ $paraText = str_replace("\n", "</p><p>", $bodyText);
     <?php echo $paraText ?>
 </p>
 
-    <h3><?php echo countCommentsForPost($postId) ?> comments</h3>
-    <?php foreach (getCommentsForPost($postId) as $comment): ?>
-        <?php // For now, we'll use a horizontal rule-off to split it up a bit ?>
-        <hr />
-        <div class="comment">
-            <div class="comment-meta">
-                Comment from
-                <?php echo htmlEscape($comment['name']) ?>
-                on
-                <?php echo convertSqlDate($comment['created_at']) ?>
-            </div>
-            <div class="comment-body">
-                <?php echo htmlEscape($comment['text']) ?>
-            </div>
+<h3><?php echo countCommentsForPost($postId) ?> comments</h3>
+
+<?php foreach (getCommentsForPost($postId) as $comment): ?>
+    <?php // For now, we'll use a horizontal rule-off to split it up a bit ?>
+    <hr />
+    <div class="comment">
+        <div class="comment-meta">
+            Comment from
+            <?php echo htmlEscape($comment['name']) ?>
+            on
+            <?php echo convertSqlDate($comment['created_at']) ?>
         </div>
-    <?php endforeach ?>
+        <div class="comment-body">
+            <?php echo htmlEscape($comment['text']) ?>
+        </div>
+    </div>
+<?php endforeach ?>
+
+<?php require 'templates/comment-form.php' ?>
 </body>
 </html>
