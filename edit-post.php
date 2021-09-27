595d7f181d3f30402e24232021c67d52f93f046d
Anastasia Isaeva <anastasiaisaeva@Anastasias-MBP.attlocal.net>
2021-09-27 11:01:32 -0500

    step 8 edit post

--- edit-post.php
+++ edit-post.php
@@ -1,11 +1,52 @@
 <?php
 require_once 'lib/common.php';
+require_once 'lib/edit-post.php';
+
 session_start();
 
 // Don't let non-auth users see this screen
 if (!isLoggedIn())
 {
     redirectAndExit('index.php');
+}
+
+// Handle the post operation here
+$errors = array();
+if ($_POST)
+{
+    // Validate these first
+    $title = $_POST['post-title'];
+    if (!$title)
+    {
+        $errors[] = 'The post must have a title';
+    }
+    $body = $_POST['post-body'];
+    if (!$body)
+    {
+        $errors[] = 'The post must have a body';
+    }
+
+    if (!$errors)
+    {
+        $pdo = getPDO();
+        $userId = getAuthUserId($pdo);
+        $postId = addPost(
+            getPDO(),
+            $title,
+            $body,
+            $userId
+        );
+
+        if ($postId === false)
+        {
+            $errors[] = 'Post operation failed';
+        }
+    }
+
+    if (!$errors)
+    {
+        redirectAndExit('edit-post.php?post_id=' . $postId);
+    }
 }
 
 ?>
@@ -17,28 +58,39 @@ if (!isLoggedIn())
 </head>
 <body>
 <?php require 'templates/title.php' ?>
+
+<?php if ($errors): ?>
+    <div class="error box">
+        <ul>
+            <?php foreach ($errors as $error): ?>
+                <li><?php echo $error ?></li>
+            <?php endforeach ?>
+        </ul>
+    </div>
+<?php endif ?>
+
 <form method="post" class="post-form user-form">
     <div>
         <label for="post-title">Title:</label>
         <input
-            id="post-title"
-            name="post-title"
-            type="text"
+                id="post-title"
+                name="post-title"
+                type="text"
         />
     </div>
     <div>
         <label for="post-body">Body:</label>
         <textarea
-            id="post-body"
-            name="post-body"
-            rows="12"
-            cols="70"
+                id="post-body"
+                name="post-body"
+                rows="12"
+                cols="70"
         ></textarea>
     </div>
     <div>
         <input
-            type="submit"
-            value="Save post"
+                type="submit"
+                value="Save post"
         />
     </div>
 </form>
