2a7985d4a12f9a94c63819f5ee261e0d70253021
Anastasia Isaeva <anastasiaisaeva@Anastasias-MBP.attlocal.net>
2021-09-27 19:55:03 -0500

    step 10

--- index.php
+++ index.php
@@ -1,66 +1,58 @@
 <?php
 require_once 'lib/common.php';
+
 session_start();
 
 // Connect to the database, run a query, handle errors
 $pdo = getPDO();
-$stmt = $pdo->query(
-    'SELECT
-       id, title, created_at, body
-    FROM
-        post
-    ORDER BY
-        created_at DESC'
-);
-if ($stmt === false)
-{
-    throw new Exception('There was a problem running this query');
-}
+$posts = getAllPosts($pdo);
 
 $notFound = isset($_GET['not-found']);
 
 ?>
 <!DOCTYPE html>
-<html lang="us">
+<html>
 <head>
     <title>A blog application</title>
     <?php require 'templates/head.php' ?>
 </head>
 <body>
-    <?php require 'templates/title.php' ?>
+<?php require 'templates/title.php' ?>
 
-    <?php if ($notFound): ?>
-        <div class="error box">
-            Error: cannot find the requested blog post
-        </div>
-    <?php endif ?>
-
-    <div class="post-list">
-        <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
-            <div class="post-synopsis">
-                <h2>
-                    <?php echo htmlEscape($row['title']) ?>
-                </h2>
-                <div class="meta">
-                    <?php echo convertSqlDate($row['created_at']) ?>
-                    (<?php echo countCommentsForPost($pdo, $row['id']) ?> comments)
-                </div>
-                <p>
-                    <?php echo htmlEscape($row['body']) ?>
-                </p>
-                <div class="post-controls">
-                    <a
-                            href="view-post.php?post_id=<?php echo $row['id'] ?>"
-                    >Read more...</a>
-                    <?php if (isLoggedIn()): ?>
-                        |
-                        <a
-                                href="edit-post.php?post_id=<?php echo $row['id'] ?>"
-                        >Edit</a>
-                    <?php endif ?>
-                </div>
-            </div>
-        <?php endwhile ?>
+<?php if ($notFound): ?>
+    <div class="error box">
+        Error: cannot find the requested blog post
     </div>
+<?php endif ?>
+
+<div class="post-list">
+    <?php foreach ($posts as $post): ?>
+        <div class="post-synopsis">
+            <h2>
+                <?php echo htmlEscape($post['title']) ?>
+            </h2>
+            <div class="meta">
+                <?php echo convertSqlDate($post['created_at']) ?>
+
+                (<?php echo countCommentsForPost($pdo, $post['id']) ?> comments)
+            </div>
+            <p>
+                <?php echo htmlEscape($post['body']) ?>
+            </p>
+            <div class="post-controls">
+                <a
+                        href="view-post.php?post_id=<?php echo $post['id'] ?>"
+                >Read more...</a>
+                <?php if (isLoggedIn()): ?>
+                    |
+                    <a
+                            href="edit-post.php?post_id=<?php echo $post['id'] ?>"
+                    >Edit</a>
+                <?php endif ?>
+            </div>
+        </div>
+    <?php endforeach ?>
+</div>
+
 </body>
 </html>
